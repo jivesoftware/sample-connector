70d02db618f65de2835ca191f6f93d9d70c8438c
Aron Racho <aron.racho@jivesoftware.com>
2015-03-29 22:39:35 -0700

    work

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,6 +1,5 @@
 clear
 
-echo "Lock table:"
 psql -U postgres -h localhost "sample-connector" -c "select * from owners order by ownerid";
 
 echo "Total number of workqueue/processed items:"
@@ -16,7 +15,7 @@ echo "Total number of processed items by worker:"
 psql -U postgres -h localhost "sample-connector" -c "select workerid,count(*) from activitylog group by workerid order by workerid";
 
 echo "Number of work items handled concurrently by the same worker (we want this to be 0):"
-psql -U postgres -h localhost "sample-connector" -c "select count(*) from activitylog group by workerid,ownerid,modtime having count(*) > 1";
+psql -U postgres -h localhost "sample-connector" -c "select count(*) from (select modtime, count(workerid) from activitylog group by workerid, ownerid,modtime having count(workerid) > 1) as duplicate;";
 
 sleep 1s
 ./watch.sh
