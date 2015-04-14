cf929ea0813d889bad6e1cf926d956f4c9785559
Aron Racho <aron.racho@jivesoftware.com>
2015-04-13 22:19:10 -0700

    modified producter script

--- scripts/producer.sh
+++ scripts/producer.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export RDS_HOSTNAME=localhost
+export RDS_PORT=5432
+export RDS_DB_NAME=sample-connector
+export RDS_USERNAME=postgres
+export RDS_PASSWORD=postgres
+
 export __ROLE=producer
 
 echo "Starting producer"
